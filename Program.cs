b54565a021da6dfb649ca083d98b505f9bca2eed
kimkaun <s2302@e-mirim.hs.kr>
2025-06-12 14:48:00 +0900

    하이딩 예(명시적으로 new 붙임)

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ namespace ClassExample
         class Child : Parent
         {
             public string variable = "하이딩";
-            public void Method() { Console.WriteLine("자식의 메서드"); }
+            public new void Method() { Console.WriteLine("자식의 메서드"); }
             public void CounterChild()
             {
                 Child.counter++;
